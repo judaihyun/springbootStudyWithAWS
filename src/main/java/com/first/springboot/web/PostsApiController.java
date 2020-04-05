2e58a37699394201949dc51d7245ce2e2bf65705
daihyun <judaihyun1@gmail.com>
2020-04-06 00:30:46 +0900

    3장까지 완료

---
+++ src/main/java/com/first/springboot/web/PostsApiController.java
@@ -0,0 +1,33 @@
+package com.first.springboot.web;
+
+import com.first.springboot.service.posts.PostsService;
+import com.first.springboot.web.dto.PostsResponseDto;
+import com.first.springboot.web.dto.PostsSaveRequestDto;
+import com.first.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        System.out.println("---- PostsApiController - save() ----");
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id,
+                       @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
+    }
+
+}
