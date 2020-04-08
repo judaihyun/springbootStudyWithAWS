c66d7dacc7a71e8ef28a151ec4a1b35dcdba51e7
daihyun <judaihyun1@gmail.com>
2020-04-08 21:22:00 +0900

    5장까지 Naver로그인 관련 에러 수정

--- src/main/java/com/first/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/first/springboot/config/auth/dto/OAuthAttributes.java
@@ -48,19 +48,15 @@ public class OAuthAttributes {
                 build();
     }
 
-    private static OAuthAttributes ofNaver(String userNameAttributeName,
-                                            Map<String, Object> attributes){
-        System.out.println("OAuthAttributes - ofNaver");
-        Map<String, Object> response =
-                (Map<String, Object>)attributes.get("response");
-
-        return OAuthAttributes.builder().
-                name((String)attributes.get("name")).
-                email((String)attributes.get("email")).
-                picture((String)attributes.get("profile_image")).
-                attributes(response).
-                nameAttributeKey(userNameAttributeName).
-                build();
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profileImage"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     public User toEntity(){
